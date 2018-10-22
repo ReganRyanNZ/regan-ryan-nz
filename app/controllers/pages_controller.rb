b74d17c7353b3331750ab0e9ad5a8f469387e185
Regan Ryan <regan.ryan.nz@gmail.com>
2018-10-22 18:16:05 +1000

    made online taboo cards

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,8 +3,17 @@ class PagesController < ApplicationController
   def index
   end
 
-  def taboo
-    @cards = CSV.new(Taboo.data).map { |matrix| matrix.map(&:strip) }
+  def taboo_print
+    @cards = get_taboo_csv
   end
 
+  def taboo
+    @cards = get_taboo_csv.to_a
+  end
+
+  private
+
+  def get_taboo_csv
+    CSV.new(Taboo.data).map { |matrix| matrix.map(&:strip) }
+  end
 end
--- app/helpers/taboo_data.rb
+++ app/helpers/taboo_data.rb
@@ -1,86 +1,92 @@
 @taboo_data = <<~CSV
-      Abraham, Lot, Faith, Called, Father, Israel
-      Adam, Eve, First, Genesis, Man, Fruit
-      Angel, Gabriel, Michael, Messenger, God, Spirit
-      Apostle, Twelve, Jesus, Disciple, John, James
-      Armor of God, Sword, Shield, Breastplate, Helmet, Ephesians
-      Babel, Tower, God, Language, Fall, Build
-      Baptism, Buried, Transfer, Declaration, Water, Salvation
-      Bethel, Jacob, Dream, Stone, Ladder, Angel
-      Bible, Word, Logos, Rhema, Breath, Life
-      Church, Building, Chrisian, Believers, Sunday / Lord's day, Hymns
-      Commandment, Rule, Obey, Ten, Moses, Law
-      Consecration, Vow, Give, Life, Lord, Future
-      Crucifixion, Death, Blood, Water, Human Living, Buried
-      Daniel, Companions, Idols, Fire, Lion, Nebuchadnezzar
-      David, Goliath, King, Saul, Psalms, Shepherd
-      Donkey, Jesus, Talking, Animal, Balaam, Mule
-      Egypt, Nile, Israel, Moses, Pharaoh, Chariot
-      Eternity, Time, Everlasting, End, God, Forever
-      Faith, Hope, Believe, See, Heart, God
-      Fellowship, Mutual, Life, Coordination, Brothers, Building
-      Fruit of the Spirit, Love, Joy, Peace, Kindness, Goodness
-      Genesis, first, beginning, Adam/Eve, eden, creation
-      God, Triune, Creator, Redeemer, Jesus, Spirit
-      God's Economy, Plan, Law, Dispense, Desire, Eternity
-      Gospel, Witness, Save, Preach, Good news, Bible
-      Grace, Enjoy, God, Christ, Peace, Spirit
-      Guitar, Sing, Play, Music, Hymn, Instrument
-      Hallelujah, Praise, Shout, Lord, God, Worship
-      Heart, Soul, Love, Hope, Faith, Organ
-      Incarnation, Man, Jesus, Earth, Process, Living
-      Israel, Jerusalem, Land, Country, People, Children
-      Jericho, Joshua, Seven, Trumpet, Wall, Rahab
-      Jesus, Lord, God, Spirit, Christ, Lamb
-      Jonah, Whale, Fish, Prophet, Ninevah, Boat
-      Joy, Happy, Rejoice, Sing, Dance, Cheerful
-      Jubilee, Fifty, Freedom, Slave, Property, Joy
-      Lamb, Jesus, Christ, Sacrifice, Spotless, Animal
-      Lampstand, Revelation, Golden, Tabernacle, Church, Expression
-      Lazarus, Resurrection, Bethany, Mary, Martha, Dead
-      Life, Resurrection, Death, Victory, Breath, Essential
-      Light, Life, John, Darkness, Revelation, Unveil
-      Lot, Sodom, Gomorrah, Wife, Abraham, Salt
-      Love, God, Brother/Sister, Heart, Commandment, Emotion
-      Manna, Bread, Israel, Wilderness, God, Jesus
-      Morning Revival, Wake, Early, Word, Pray, Contact
-      Moses, Red Sea, Exodus, Israel, Aaron, Staff
-      New Jerusalem, City, Gold, Descend, Revelation, John
-      Olive Oil, Spirit, Anointing, Aaron, Cook, Priest
-      Oneness, Love, Brothers, Fellowship, Coordination, Together
-      Overcomer, Choice, Love, Turn, Revelation, Call
-      Parable, Story, Jesus, Seed, Coin, Lesson
-      Passover, Plague, Egypt, Blood, Lamb, Israel
-      Paul, Apostle, Saul, New Testament, Epistle, Timothy
-      Peter, Apostle, Disciple, Fishing, Gospel, Epistle
-      Praise, Worship, Lord, Hands, Thanks, Pray
-      Pray, Call, Praise, Speak, God, Lord
-      Priest, God, Serve, Levite, Aaron, Israel
-      Prodigal, Son, Father, Robe, Calf, Luke
-      Regeneration, Born, Receive, Believe, Divine, Life
-      Resurrection, Life, Dead, Jesus, Days, Tomb
-      River, Life, Water, Spirit, Believe, Flow
-      Salvation, Believe, Receive, Regenerated, God, Sinner
-      Samaritan, Good, Water, Donkey, Well, Inn
-      Samson, Strong, Delilah, Nazarite, Hair, Lion
-      Samuel, Hannah, Nazarite, Eli, Prophet, David
-      Shepherd, Sheep, Love, Lead, Care, Companion
-      Sin, Satan, Repent, Blood, Evil, Forgiveness
-      Sing, Psalm, Music, Voice, Meeting, Sound
-      Solomon, Wise, King, David, Temple, Christ
-      Stephen, Stoned, Martyr, Forgive, Death, Saul
-      Tabernacle, Tent of meeting, Holy of Holies, Holy Place, Outer court, ark
-      Temple, House, God, Jerusalem, Building, Solomon
-      Tree of Life, Eden, Adam / Eve, Vine, Knowledge, Fruit
-      Truth, Lie, Bible, Word, Honest, Faith
-      Vessel, God, Spirit, Man, Container, Mystery
-      Vineyard, Wine, Grapes, Juice, Fruit, Press
-      Mt of Olives, Jerusalem, Mountain, Jesus, Return, Ascension
-      Sea of galiee, Apostles, Water, Peter, Faith, Storm
-      Armageddon, Battle, End, Overcomers, Nations, Coming
-      High Priest, Holy, Temple, Sins, Offering, Aaron
-      Passover, Plague, Lamb, Angel, Death, Blood
-      Locust, Insect, Plague, Egypt, Moses, Pharaoh
-      Good Land, Israel, Canaan, Milk, Honey, Desert
-      Rahab, Scarlet cord, Spies, Jericho, Harlot, Walls
-    CSV
+  Abel, Cain, Offering, Brother, Adam, Eve
+  Abraham, Lot, Faith, Called, Father, Israel
+  Adam, Eve, First, Genesis, Man, Fruit
+  Angel, Gabriel, Michael, Messenger, God, Spirit
+  Apostle, Twelve, Jesus, Disciple, John, James
+  Armor of God, Sword, Shield, Breastplate, Helmet, Ephesians
+  Babel, Tower, God, Language, Fall, Build
+  Baptism, Buried, Transfer, Declaration, Water, Salvation
+  Bethel, Jacob, Dream, Stone, Ladder, Angel
+  Bible, Word, Logos, Rhema, Breath, Life
+  Cain, Abel, Offering, Brother, Adam, Eve
+  Church, Building, Christian, Believers, Sunday / Lord's day, Hymns
+  Commandment, Rule, Obey, Ten, Moses, Law
+  Consecration, Vow, Give, Life, Lord, Future
+  Crucifixion, Death, Blood, Water, Human Living, Buried
+  Daniel, Companions, Idols, Fire, Lion, Nebuchadnezzar
+  David, Goliath, King, Saul, Psalms, Shepherd
+  Donkey, Jesus, Talking, Animal, Balaam, Mule
+  Egypt, Nile, Israel, Moses, Pharaoh, Chariot
+  Eternity, Time, Everlasting, End, God, Forever
+  Faith, Hope, Believe, See, Heart, God
+  Fellowship, Mutual, Life, Coordination, Brothers, Building
+  Fruit of the Spirit, Love, Joy, Peace, Kindness, Goodness
+  Genesis, first, beginning, Adam/Eve, eden, creation
+  God, Triune, Creator, Redeemer, Jesus, Spirit
+  God's Economy, Plan, Law, Dispense, Desire, Eternity
+  Gospel, Witness, Save, Preach, Good news, Bible
+  Grace, Enjoy, God, Christ, Peace, Spirit
+  Guitar, Sing, Play, Music, Hymn, Instrument
+  Hallelujah, Praise, Shout, Lord, God, Worship
+  Heart, Soul, Love, Hope, Faith, Organ
+  Incarnation, Man, Jesus, Earth, Process, Living
+  Israel, Jerusalem, Land, Country, People, Children
+  Jacob, Israel, Wrestle, Isaac, Abraham, Joseph
+  Jericho, Joshua, Seven, Trumpet, Wall, Rahab
+  Jesus, Lord, God, Spirit, Christ, Lamb
+  Jonah, Whale, Fish, Prophet, Ninevah, Boat
+  Joseph, Jacob, Twelve, Coat, Egypt, Tribe
+  Joy, Happy, Rejoice, Sing, Dance, Cheerful
+  Jubilee, Fifty, Freedom, Slave, Property, Joy
+  King, David, Saul, Christ, Crown, Ruler
+  Lamb, Jesus, Christ, Sacrifice, Spotless, Animal
+  Lampstand, Revelation, Golden, Tabernacle, Church, Expression
+  Lazarus, Resurrection, Bethany, Mary, Martha, Dead
+  Life, Resurrection, Death, Victory, Breath, Essential
+  Light, Life, John, Darkness, Revelation, Unveil
+  Lot, Sodom, Gomorrah, Wife, Abraham, Salt
+  Love, God, Brother/Sister, Heart, Commandment, Emotion
+  Manna, Bread, Israel, Wilderness, God, Jesus
+  Morning Revival, Wake, Early, Word, Pray, Contact
+  Moses, Red Sea, Exodus, Israel, Aaron, Staff
+  New Jerusalem, City, Gold, Descend, Revelation, John
+  Olive Oil, Spirit, Anointing, Aaron, Cook, Priest
+  Oneness, Love, Brothers, Fellowship, Coordination, Together
+  Overcomer, Choice, Love, Turn, Revelation, Call
+  Parable, Story, Jesus, Seed, Coin, Lesson
+  Passover, Plague, Egypt, Blood, Lamb, Israel
+  Paul, Apostle, Saul, New Testament, Epistle, Timothy
+  Peter, Apostle, Disciple, Fishing, Gospel, Epistle
+  Praise, Worship, Lord, Hands, Thanks, Pray
+  Pray, Call, Praise, Speak, God, Lord
+  Priest, God, Serve, Levite, Aaron, Israel
+  Prodigal, Son, Father, Robe, Calf, Luke
+  Regeneration, Born, Receive, Believe, Divine, Life
+  Resurrection, Life, Dead, Jesus, Days, Tomb
+  River, Life, Water, Spirit, Believe, Flow
+  Salvation, Believe, Receive, Regenerated, God, Sinner
+  Samaritan, Good, Water, Donkey, Well, Inn
+  Samson, Strong, Delilah, Nazarite, Hair, Lion
+  Samuel, Hannah, Nazarite, Eli, Prophet, David
+  Saul, David, King, Tarsus, Paul, Damascus
+  Shepherd, Sheep, Love, Lead, Care, Companion
+  Sin, Satan, Repent, Blood, Evil, Forgiveness
+  Sing, Psalm, Music, Voice, Meeting, Sound
+  Solomon, Wise, King, David, Temple, Christ
+  Stephen, Stoned, Martyr, Forgive, Death, Saul
+  Tabernacle, Tent of meeting, Holy of Holies, Holy Place, Outer court, ark
+  Temple, House, God, Jerusalem, Building, Solomon
+  Tree of Life, Eden, Adam / Eve, Vine, Knowledge, Fruit
+  Truth, Lie, Bible, Word, Honest, Faith
+  Vessel, God, Spirit, Man, Container, Mystery
+  Vineyard, Wine, Grapes, Juice, Fruit, Press
+  Mt of Olives, Jerusalem, Mountain, Jesus, Return, Ascension
+  Sea of galiee, Apostles, Water, Peter, Faith, Storm
+  Armageddon, Battle, End, Overcomers, Nations, Coming
+  High Priest, Holy, Temple, Sins, Offering, Aaron
+  Passover, Plague, Lamb, Angel, Death, Blood
+  Locust, Insect, Plague, Egypt, Moses, Pharaoh
+  Good Land, Israel, Canaan, Milk, Honey, Desert
+  Rahab, Scarlet cord, Spies, Jericho, Harlot, Walls
+CSV
