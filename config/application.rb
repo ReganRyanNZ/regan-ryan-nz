c32cd6ce52ebd099e96f98a1c9cebbac0e2cb8ff
Regan Ryan <regan.ryan.nz@gmail.com>
2018-12-21 00:29:02 +1000

    fixed assets not found on heroku

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,9 @@ module ReganRyanNz
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
 
+    # This allows assets to be found on heroku
+    config.serve_static_assets = true
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
