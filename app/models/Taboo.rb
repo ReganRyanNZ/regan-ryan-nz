d9f0fe00b78e2f05a62bcb5f7a543b394e8a4ad8
Regan <regan@crewmeister.com>
2020-02-15 13:44:35 +0100

    added words to taboo

--- app/models/Taboo.rb
+++ app/models/Taboo.rb
@@ -1,99 +1,182 @@
 class Taboo
   def self.data
     <<~CSV
+      Aaron, Miriam, Moses, brother, Priest, Levi
+      Abel, brother, tender of sheep, Cain, first, killed/murder
       Abide, Vine, Dwell, Live, Branch, Home
       Abraham, Lot, Faith, Called, Father, Israel
+      Abyss, millennium, thousand, anti-christ, Hades, dark
       Adam, Eve, First, Genesis, Man, Fruit
       Angel, Gabriel, Michael, Messenger, God, Spirit
       Apostle, Twelve, Jesus, Disciple, John, James
+      Ark of the Covenant, budding rod, tablets, hidden manna, golden pot, Holy of Holies
       Armageddon, Battle, End, Overcomers, Nations, Coming
       Armor of God, Sword, Shield, Breastplate, Helmet, Ephesians
       Babel, Tower, God, Language, Fall, Build
       Baptism, Buried, Transfer, Declaration, Water, Salvation
+      Benjamin, Jacob, 12, sons, Rachel, Joseph
       Bethel, Jacob, Dream, Stone, Ladder, Angel
       Bible, Word, Logos, Rhema, Breath, Life
+      Bios, physical, body, life, three, temporary
+      Body of Christ, church, fullness, baptised, members, enlargement
+      Bronze Serpent, Moses, lifted up, wilderness, poison, evil nature
+      Burnt offering, Leviticus, five, animal, altar, nothing
+      Cain, murder, offered, tiller, ground, brother
+      Chebar, strong, powerful, Babylon, river, Ezekiel
+      Chosen, foundation, world, before, universe, God
       Church, Building, Chrisian, Believers, Sunday / Lord's day, Hymns
       Commandment, Rule, Obey, Ten, Moses, Law
       Consecration, Vow, Give, Life, Lord, Future
       Crucifixion, Death, Blood, Water, Human Living, Buried
+      Dan, Jacob, 12, sons, serpent, viper
       Daniel, Companions, Idols, Fire, Lion, Nebuchadnezzar
       David, Goliath, King, Saul, Psalms, Shepherd
       Donkey, Jesus, Talking, Animal, Balaam, Mule
+      Effulgence, shining, brightness, Glory, God, Impress
       Egypt, Nile, Israel, Moses, Pharaoh, Chariot
       Elijah, Prophet, Miracle, Tent, Moses, Transfiguration
+      Enoch, 300, walked, die, rapture, Elijah
+      Enosh, call, name, Lord, frail, mortal
+      Ephesus, desirable, first love, church, seven, revelation
+      Esther, Mordecai, cousin, queen, God, book
       Eternity, Time, Everlasting, End, God, Forever
+      Ezra, house, Jerusalem, captives, build, Babylon
       Faith, Hope, Believe, See, Heart, God
+      Father's house, abodes, John 14, prepare, place, believers
       Fellowship, Mutual, Life, Coordination, Brothers, Building
+      Firstborn Son, resurrection, divinity, humanity, two, natures
+      Four Living Creatures, Ezekiel, Man, Ox, Eagle, Lion
       Fruit of the Spirit, Love, Joy, Peace, Kindness, Goodness
+      Fullness, Body of Christ, overflowing, all, eaten, bloated
+      Garden of Eden, Genesis, Adam, Even, Satan, Fell
       Genesis, first, beginning, Adam/Eve, eden, creation
+      Glory, God expressed, shining, transfiguration, righteousness, holiness
       God, Triune, Creator, Redeemer, Jesus, Spirit
       God's Economy, Plan, Law, Dispense, Desire, Eternity
       Good Land, Israel, Canaan, Milk, Honey, Desert
       Gospel, Witness, Save, Preach, Good news, Bible
       Grace, Enjoy, God, Christ, Peace, Spirit
+      Grain of wheat, life releasing, dies, fruit, falls, ground
       Guitar, Sing, Play, Music, Hymn, Instrument
+      Hagar, Maidservent, Sarah, Wilderness, Egyptian, Abraham
       Hallelujah, Praise, Shout, Lord, God, Worship
       Hannah, Pray, Samuel, Son, Eli, Temple
       Heart, Soul, Love, Hope, Faith, Organ
       High Priest, Holy, Temple, Sins, Offering, Aaron
+      Holy of Holies, tabernacle, ark, contact God, high priest, spirit
+      Holy Place, lampstand, incense altar, table of the bread of the presence, showbread, tabernacle
+      Holy, nature, separated, distinct, set apart, sanctify
       Idol, God, Calf, Golden, Heart, Worship
       Incarnation, Man, Jesus, Earth, Process, Living
+      Isaac, laughter, Christ, Abraham, mountain, Genesis
+      Ishmael, Isaac, flesh, self-effort, Abraham, Hagar
       Israel, Jerusalem, Land, Country, People, Children
+      Jacob, Rachel, Israel, 12, wrestle, Isaac
       Jericho, Joshua, Seven, Trumpet, Wall, Rahab
       Jesus, Lord, God, Spirit, Christ, Lamb
       John the Baptist, Water, Baptize, Jordan, Forerunner, River
       Jonah, Whale, Fish, Prophet, Ninevah, Boat
+      Jonathan, David, friend, Saul, crippled feet, son
+      Jordan, Israel, crossed, good land, stones, river
+      Joseph, coat, colours, dream, baker, cup-bearer
       Joy, Happy, Rejoice, Sing, Dance, Cheerful
       Jubilee, Fifty, Freedom, Slave, Property, Joy
+      Lake of fire, Satan, End, death, unbelievers, burning
       Lamb, Jesus, Christ, Sacrifice, Spotless, Animal
       Lampstand, Revelation, Golden, Tabernacle, Church, Expression
+      Laodicea, seven, church, revelation, lukewarm, knock
+      Law of the Spirit of Life, sin, death, opposite, higher, freed
       Lazarus, Resurrection, Bethany, Mary, Martha, Dead
+      Levi, priesthood, allotted, portion, land, service
       Life, Resurrection, Death, Victory, Breath, Essential
       Light, Life, John, Darkness, Revelation, Unveil
       Locust, Insect, Plague, Egypt, Moses, Pharaoh
       Lot, Sodom, Gomorrah, Wife, Abraham, Salt
       Love, God, Brother/Sister, Heart, Commandment, Emotion
       Manna, Bread, Israel, Wilderness, God, Jesus
+      Matthew, first, book, New Testament, tax collector, money
+      Meal offering, Leviticus, five, bake, flour, oil
+      Mercy, rich, great love, receive, grace, ask
+      Mingling, meal, offering, spirit, divine, human
+      Miriam, leprosy, rebellion, Moses, Aaron, prophetess
       Morning Revival, Wake, Early, Word, Pray, Contact
       Moses, Red Sea, Exodus, Israel, Aaron, Staff
       Mt of Olives, Jerusalem, Mountain, Jesus, Return, Ascension
+      Nebuchadnezzar, Daniel, dream, image, Babylon, king
       New Jerusalem, City, Gold, Descend, Revelation, John
+      New, buy, not used, second-hand, in Christ, creation
+      Nile, river, blood, Egypt, frogs, water
+      Noah, ark, comfort, rest, boat, rainbow
+      Noah's ark, Noah, eight, resurrection, flood, boat
       Olive Oil, Spirit, Anointing, Aaron, Cook, Priest
+      One New Man, created, cross, Jews, Gentiles, Colossians 3:11
       Oneness, Love, Brothers, Fellowship, Coordination, Together
+      Only begotten Son, divinity, opposite, no humanity, incarnate, invisible
+      Outer Court, Bronze altar, laver, sacrifice, tabernacle, offerings
       Overcomer, Choice, Love, Turn, Revelation, Call
       Parable, Story, Jesus, Seed, Coin, Lesson
       Passover, Plague, Egypt, Blood, Lamb, Israel
       Passover, Plague, Lamb, Angel, Death, Blood
       Paul, Apostle, Saul, New Testament, Epistle, Timothy
+      Peace offering, Leviticus, five, altar, animal, war
+      Peacemaker, one new man, abolish, Jews, Gentiles, culture
+      Pergamos, world, hidden manna, church, seven, revelation
       Peter, Apostle, Disciple, Fishing, Gospel, Epistle
       Pharisee, Temple, Law, Religion, Jewish, Leaders
+      Philadelphia, seven, church, revelation, brotherly love, pillar
       Praise, Worship, Lord, Hands, Thanks, Pray
       Pray, Call, Praise, Speak, God, Lord
       Predestination, Chosen, Sonship, Before, Eternity, Foreknown
       Priest, God, Serve, Levite, Aaron, Israel
       Prodigal, Son, Father, Robe, Calf, Luke
       Prophet, Elijah, Moses, Dreams, Visions, Prophecy
+      Propitiation, appease, expiate, party, blood, Lord Jesus
+      Psuche, psychology, mind, soul, life, three
       Rahab, Scarlet cord, Spies, Jericho, Harlot, Walls
+      Reap, harvest, produce, gather, sow, farm
+      Reconciliation, friends, enemy, enmity, parties, all good
+      Red sea, crossed, Israel, Moses, Egyptians, split/parted
+      Redemption, price, pay, cross, blood, die
       Regeneration, Born, Receive, Believe, Divine, Life
+      Reign in life, abundance, grace, righteousness, king, mature
       Resurrection, Life, Dead, Jesus, Days, Tomb
+      Reuben, Jacob, 12, sons, firstborn, birthright
+      Righteous, Love, grace, Judicial, Acts, Way
       River, Life, Water, Spirit, Believe, Flow
       Ruth, Boaz, Naomi, Field, Moab, Mother-in-law
       Salvation, Believe, Receive, Regenerated, God, Sinner
       Samaritan, Good, Water, Donkey, Well, Inn
       Samson, Strong, Delilah, Nazarite, Hair, Lion
       Samuel, Hannah, Nazarite, Eli, Prophet, David
+      Sanctification, saturation, spreading, holy, separated, world
+      Sardis, seven, church, revelation, reformation, dead
       Sea of Galiee, Apostles, Water, Peter, Faith, Storm
+      Sheol, Hades, Death, Keeper, Old Testament, die
       Shepherd, Sheep, Love, Lead, Care, Companion
+      Sin offering, Leviticus, five, Satan, evil, altar
       Sin, Satan, Repent, Blood, Evil, Forgiveness
       Sing, Psalm, Music, Voice, Meeting, Sound
+      Smyrna, suffering, persecution, crown of life, revelation, church
       Solomon, Wise, King, David, Temple, Christ
+      Son of God, divinity, incarnate, Lord Jesus Christ, flesh, man
+      Son of Man, humanity, incarnate, flesh, Lord Jesus Christ, earth
+      Sow, seed, earth, farm, grow, reap
       Stephen, Stoned, Martyr, Forgive, Death, Saul
       Tabernacle, Tent of meeting, Holy of Holies, Holy Place, Outer court, ark
       Temple, House, God, Jerusalem, Building, Solomon
+      Thyatira, catholic, apostasy, seven, revelation, church
+      Timothy, Greek father, Timothy 2:22, Lois, Eunice, church kid
+      Transformation, renewing, butterfly, metabolic, change, metamorphosis
       Tree of Life, Eden, Adam / Eve, Vine, Knowledge, Fruit
+      Trespass offering, Leviticus, five, forgiveness, altar, animal
+      Triune God, Father, Son, Spirit, three, one
       Truth, Lie, Bible, Word, Honest, Faith
+      Union, joined, one spirit, Lord, divine, human
       Vessel, God, Spirit, Man, Container, Mystery
+      Vine, branch, abide, bear fruit, nothing, John 15
       Vineyard, Wine, Grapes, Juice, Fruit, Press
+      Zion, temple, David, mountain, hill, city
+      Zoe, divine, indestructible, uncreated, eternal, life
     CSV
   end
 end
