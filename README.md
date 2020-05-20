e1d98fab57d01e9f64c6e0490932caf70adb473b
Regan Ryan <regan.ryan.nz@gmail.com>
2020-05-21 00:26:30 +1200

    added scotland

--- README.md
+++ README.md
@@ -1,24 +1,4 @@
-# README
+This is my personal site, a bit of a sandbox.
 
-This README would normally document whatever steps are necessary to get the
-application up and running.
+Note to self, resizing batch images can be done with this https://workflowy.com/#/884abe7a071b
 
-Things you may want to cover:
-
-* Ruby version
-
-* System dependencies
-
-* Configuration
-
-* Database creation
-
-* Database initialization
-
-* How to run the test suite
-
-* Services (job queues, cache servers, search engines, etc.)
-
-* Deployment instructions
-
-* ...
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,23 +1,32 @@
 module ApplicationHelper
 
-  def blog_image_tag ref, title: ""
-    html = <<~HTML
-    <div class='img'>
-      #{image_tag(ref, alt: title)}
-      <div class='caption'>#{title}</div>
-    </div>
-    HTML
-    html.html_safe
-  end
-
-  def blog_hero_image_tag ref, title: ""
-    html = <<~HTML
-    <div class='img-hero'>
-      #{image_tag(ref, alt: title)}
-      <div class='caption'>#{title}</div>
-    </div>
-    HTML
-    html.html_safe
+  def blog_image_tag ref, title: "", size: 'square'
+    case size
+    when 'square'
+      html = <<~HTML
+      <div class='img'>
+        #{image_tag(ref, alt: title)}
+        <div class='caption'>#{title}</div>
+      </div>
+      HTML
+      html.html_safe
+    when 'hero'
+      html = <<~HTML
+      <div class='img-hero'>
+        #{image_tag(ref, alt: title)}
+        <div class='caption'>#{title}</div>
+      </div>
+      HTML
+      html.html_safe
+    when 'full'
+      html = <<~HTML
+      <div class='img-full'>
+        #{image_tag(ref, alt: title)}
+        <div class='caption'>#{title}</div>
+      </div>
+      HTML
+      html.html_safe
+    end
   end
 
   def blog_index url_id, title: "", location: "", date: ""
